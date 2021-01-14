593833f52d5e636c1d959c81b268ee1f43b5b8e3
Jefferson Alvarez <jeffersonalvarezalvarez@gmail.com>
2021-01-13 19:26:59 -0500

    Added devise authentication and layout design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require('jquery/dist/jquery.min')
+require('popper.js/dist/popper.min')
+require('bootstrap/dist/js/bootstrap.min')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +19,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+toastr = require("toastr")
